299d75facd991d66bba693d43e5cc1cb43e50ff8
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-06-28 19:51:47 -0500

    compared to snapshot

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
     // This...
@@ -8,10 +7,8 @@ const printProfileData = profileDataArr => {
     }
     console.log('================');
 
-    // Is the same as this...
    // for Each loop
    profileDataArr.forEach(profileItem => console.log(profileItem));
-   console.log(profileItem)
 };
 
 printProfileData(profileDataArgs);
