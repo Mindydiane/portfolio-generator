07e1b1e120d932d6a0d5aac9cc449186b13499b6
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-07-15 18:16:30 -0500

    Generated entire website complete

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const fs = require("fs");
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+
 const inquirer = require("inquirer");
 const generatePage = require("./src/page-template");
 
@@ -138,33 +139,18 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile("./index.html", pageHTML, (err) => {
-      if (err) throw new Error(err);
-
-      console.log(
-        "Page created! Check out index.html in this directory to see it!"
-      );
-    });
-
-    // const pageHTML = generatePage(name, github);
-
-    fs.writeFile("./dist/index.html", pageHTML, (err) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(
-        "Page created! Check out index.html in this directory to see it!"
-      );
-
-      fs.copyFile("./src/style.css", "./dist/style.css", (err) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log("Style sheet copied sucessfully!");
-      });
-    });
+    return generatePage(portfolioData);  
+  })
+  .then(pageHTML => {
+      return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+      console.log(writeFileResponse);
+      return copyFile();
+  })
+  .then(copyFileResponse => {
+      console.log(copyFileResponse);
+  })
+  .catch(err => {
+      console.log(err);
   });
